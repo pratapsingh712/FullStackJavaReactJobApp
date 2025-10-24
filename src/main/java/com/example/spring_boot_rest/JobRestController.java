5a4333b9962afd10612a8ba2f83a8cd9a9dca2ab
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-10-24 17:04:21 +0530

    added rest controller

--- src/main/java/com/example/spring_boot_rest/JobRestController.java
+++ src/main/java/com/example/spring_boot_rest/JobRestController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+//@Controller
+@RestController
 public class JobRestController {
 
     @Autowired
