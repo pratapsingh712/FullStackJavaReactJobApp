c34fac5fde09d997a4843c2ded6185f5eabc656b
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-10-24 17:21:28 +0530

    added cross origin for front end react application

--- src/main/java/com/example/spring_boot_rest/JobRestController.java
+++ src/main/java/com/example/spring_boot_rest/JobRestController.java
@@ -4,6 +4,7 @@ import com.example.spring_boot_rest.model.JobPost;
 import com.example.spring_boot_rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,11 @@ public class JobRestController {
     private JobService jobService;
 
     @GetMapping("jobPosts")
-    @ResponseBody
+//    @ResponseBody -- this is not required as I am using RestController now and it does not think below method is returning jsp by default
+    @CrossOrigin(origins = "http://localhost:3000")
     public List<JobPost> getAllJobs(){
         return jobService.getAllJobs();
     }
+
+
 }
