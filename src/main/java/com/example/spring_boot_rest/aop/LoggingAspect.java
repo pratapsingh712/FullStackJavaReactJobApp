57cf10568a923514a68c7414d500957e77e0d6d6
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-11-20 15:24:00 +0530

    added aspect orientied programming methods like @Before

--- src/main/java/com/example/spring_boot_rest/aop/LoggingAspect.java
+++ src/main/java/com/example/spring_boot_rest/aop/LoggingAspect.java
@@ -1,12 +1,19 @@
 package com.example.spring_boot_rest.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
+    //execution(return type, class-name.method-name(args)
+    @Before("execution(* com.example.spring_boot_rest.service.JobService.*(..))")
     public void logMethodCall(){
         LOGGER.info("Method called :");
     }
